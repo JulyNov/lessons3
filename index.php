7e851a81d8983d4dda8e7ea59e34c63cfcd3e098
July Nov <july@nov.so>
2017-03-17 11:37:54 +0200

    Таблица пользователей

--- index.php
+++ index.php
@@ -9,3 +9,20 @@ if (!$_SESSION['id']) {
 echo 'Привет Юзер<br>';
 echo '<a href="/logout.php">Выход</a>';
 
+$sql = $db->prepare ('SELECT id, name, age, description, email, foto FROM `Users`;');
+$sql->execute();
+
+$sql->bind_result($id, $name, $age, $description, $email, $foto);
+
+echo '<table>';
+while ($sql->fetch()) {
+    echo '<tr>';
+    printf('<td>%s</td>', $id);
+    printf('<td>%s</td>', htmlspecialchars($name));
+    printf('<td>%s</td>', htmlspecialchars($email));
+    printf('<td>%s</td>', $age);
+    printf('<td>%s</td>', htmlspecialchars($description));
+    echo '</tr>';
+
+}
+echo '</table>';
